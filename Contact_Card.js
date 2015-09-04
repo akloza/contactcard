10fd76e7efaa77fb2abe6e12cb38a3ccc91e02a2
akloza <akloza@ucdavis.edu>
2015-09-04 11:28:56 -0700

    Update Contact_Card.js

--- Contact_Card.js
+++ Contact_Card.js
@@ -1,27 +1,24 @@
-function showDescription(){
-	$('#contact_card').click(function(){
-		$('#contact_card').append('.activeDescription');
-	});
-}
-
 $(document).ready(function(){
-	showDescription();
 
-	$('button').click(function(){
-		var append=$('<div id="contact_card"></div>');
-			$(this).parent().append(append);
-		var array=[];
-	
-	$(".activeInput").each(function(){
-		array[$(this).attr("name")]=$(this).val();
-		$('#contact_card').append($(this).val());
-		$('input').focus(function(){
-		$(this).css('outline-color', '#FF0000');
 
-		showDescription();
-			});	
-		});
-	});
+    $('button').click(function(){
+
+        var new_card = '<div class="contact_card">'
+        new_card += '<span class="name">'+$("#firstname").val() + " " + $("#lastname").val() + 
+        '</span><span class="description" style="display:none">'+ $("#description").val() +
+         '</span></div>';
+        // Add the new card we made to the parent
+        $(this).parent().append(new_card);
+    });
+    
+    
+    //
+    $(document).on("click",".contact_card",function() {
+        //This function will apply eto every contact card.
+         $(this).children().toggle();
+    });
+    //
+    
 });
 
 
